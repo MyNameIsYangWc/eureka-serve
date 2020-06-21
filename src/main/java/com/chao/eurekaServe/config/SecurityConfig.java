b584434e11cf4c34a05dbc4b30cc0be5e23fa849
MyNameIsYwc <1414702504@qq.com>
2020-06-21 20:56:26 +0800

    放行注册服务

--- src/main/java/com/chao/eurekaServe/config/SecurityConfig.java
+++ src/main/java/com/chao/eurekaServe/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.chao.eurekaServe.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,12 +22,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
 
     /**
+     * 登录拦截过滤器配置
+     * @param http
+     * @throws Exception
+     * @author 杨文超
+     * @Date 2020-06-21
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            //禁用 csrf
+            .cors().and().csrf().disable()
+            .authorizeRequests()
+                //放行注册服务
+            .antMatchers("/eureka/apps/*").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin();
+    }
+
+    /**
      * 初始化UserDetailsService实例
      * 根据用户名获取用户信息进行登录校验
      * @return
      * @throws Exception
      * @author 杨文超
-     * @Date 2020-06-20
+     * @Date 2020-06-21
      */
     @Bean
     public UserDetailsService users() throws Exception {
